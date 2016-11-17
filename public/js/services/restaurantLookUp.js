42819615c020f7b9f5ee8307d9b1027648a66536
JulesJam <julian.wyatt@1xdconsulting.co.uk>
2016-11-17 18:08:55 +0000

    Updating the bower.json

--- public/js/services/restaurantLookUp.js
+++ public/js/services/restaurantLookUp.js
@@ -1,20 +1,19 @@
 angular
-  .module("Meet2Eat")
-  .factory("RestaurantLookUp", RestaurantLookUp);
+.module("Meet2Eat")
+.factory("RestaurantLookUp", RestaurantLookUp);
 
 RestaurantLookUp.$inject = ['User', '$http'];
 function RestaurantLookUp(User, $http) {
-
-    return {
-        getCuisines: function(location) {
-            return $http({
-                method: 'GET',
-                url: '/api/restaurants/cuisines',
-                params: location
-            })
-            .then(function(results) {
-                return results.data;
-            });
-        }
+  return {
+    getCuisines: function(location) {
+      return $http({
+        method: 'GET',
+        url: '/api/restaurants/cuisines',
+        params: location
+      })
+      .then(function(results) {
+        return results.data;
+      });
     }
+  }
 }
